3887197e6515329486ccd44c0cb5c0e9efd0c535
Strejby <77832031+Strejby@users.noreply.github.com>
2022-02-01 16:08:39 +0100

    Update.

--- pages/index.js
+++ pages/index.js
@@ -2,86 +2,43 @@ import React, { useState, useEffect } from "react";
 
 const defaultEndPoint = "https://swapi.dev/api/people/";
 
-export async function getServerSideProps() {
-    const res = await fetch(defaultEndPoint);
-    const data = await res.json();
-    return {
-        props: {
-            data,
-        },
+export async function getData() {
+    //Utility Functions for fetch
+    const urls = [
+        "https://swapi.co/api/planets/",
+        "https://swapi.co/api/films/",
+        "https://swapi.co/api/people/",
+    ];
+    const checkStatus = (res) =>
+        res.ok
+            ? Promise.resolve(res)
+            : Promise.reject(new Error(res.statusText));
+    const parseJSON = (response) => response.json();
+
+    // Get a single endpoint.
+    const getPage = (url) =>
+        fetch(url)
+            .then(checkStatus)
+            .then(parseJSON)
+            .catch((error) => console.log("There was a problem!", error));
+
+    // Keep getting the pages until the next key is null.
+    const getAllPages = async (url, collection = []) => {
+        const { results, next } = await getPage(url);
+        collection = [...collection, ...results];
+        if (next !== null) {
+            return getAllPages(next, collection);
+        }
+        return collection;
     };
+
+    // Select data out of all the pages gotten.
+    const [planets, films, people] = await Promise.all(
+        urls.map((url) => getAllPages(url))
+    );
+    buildData(films, planets, people);
 }
 
 export default function index({ data }) {
-    const { next, results: defaultResults = [] } = data;
-    const [results, updateResults] = useState(defaultResults);
-    const [minIndex, setMinIndex] = useState(0);
-    const [maxsIndex, setMaxIndex] = useState(2);
-    const [page, updatePage] = useState({
-        current: defaultEndPoint,
-    });
-
-    const { current } = page;
-
-    useEffect(() => {
-        if (current === defaultEndPoint) return;
-
-        async function request() {
-            const res = await fetch(current);
-            const nextData = await res.json();
-            updatePage({
-                current,
-                ...nextData.info,
-            });
-
-            if (!nextData.info?.prev) {
-                updateResults(nextData.results);
-                return;
-            }
-
-            updateResults((prev) => {
-                return [...prev, ...nextData.results];
-            });
-        }
-        request();
-    }, [current]);
-
-    function handleLoadMore() {
-        setMaxIndex(maxsIndex + 1);
-    }
-
-    function handleOnSubmitSearch(e) {
-        e.preventDefault();
-
-        const { currentTarget = {} } = e;
-        const fields = Array.from(currentTarget?.elements);
-        const fieldQuery = fields.find((field) => field.name === "query");
-
-        const value = fieldQuery.value || "";
-        const endpoint = "https://swapi.dev/api/people/?search=" + value;
-
-        updatePage({
-            current: endpoint,
-        });
-    }
-
-    return (
-        <>
-            <form onSubmit={handleOnSubmitSearch}>
-                <input name="query" type="search" />
-                <button>Search</button>
-            </form>
-            {results.slice(minIndex, maxsIndex).map((result) => {
-                const { name } = result;
-                return (
-                    <li key={name}>
-                        <a>{name}</a>
-                    </li>
-                );
-            })}
-            <button type="button" onClick={handleLoadMore}>
-                Load more
-            </button>
-        </>
-    );
+    return <>{data}</>;
 }
